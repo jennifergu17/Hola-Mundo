21c2e1a464b11bd610310dae7895357a350e1717
Jennifergu17 <jennifergu17@hotmail.com>
2016-02-18 18:04:20 -0600

    Added files via upload

---
+++ gudiel.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : gudiel.c
+ Author      : jennifer Gudiel
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("!!!Hola_mundo!!!"); /* prints !!!Hola_mundo!!! */
+	return EXIT_SUCCESS;
+}
